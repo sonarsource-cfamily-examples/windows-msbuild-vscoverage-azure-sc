0758e3d8e8669fa508c072fa99888f30c7004f13
Amelie Renard <amelie.renard@sonarsource.com>
2022-04-12 16:03:10 +0200

    Add code coverage

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,8 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-  int num = argc - 1;
-
-  if (num == 0) {
-    cout << "No arguments provided\n";
-  } else if (num == 0) { // intentional mistake
-    cout << "1 argument provided\n";
-  } else if (num == 2) {
-    cout << "2 arguments provided\n";
-  } else {
-    cout << num << " arguments provided\n";
-  }
-  if (argv != 0) {
-    cout << "argv not null\n";; // intentional extra-semicolon
-  }
-  if (argv == nullptr) {
-    return **argv; // intentional nullptr dereference
-  }
-
+int main() {
+  cout << "Hello World!\n";
   return 0;
 }
 
